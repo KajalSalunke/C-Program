31aa900090e1771bd88325534c54749a9f7a64a5
KajalSalunke <105976407+KajalSalunke@users.noreply.github.com>
2022-07-16 16:30:53 +0530

    Update DisplayFactor.c

--- DisplayFactor.c
+++ DisplayFactor.c
@@ -19,6 +19,10 @@
 void DisplayFactor(int iNo)
 {
     int iCnt = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
     for (iCnt = 1; iCnt <= iNo; iCnt++)
     {
         if ((iNo % iCnt) == 0)
